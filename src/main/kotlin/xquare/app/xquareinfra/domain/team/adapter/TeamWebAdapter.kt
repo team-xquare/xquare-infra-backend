edffe12ca8ba779d02c67f920dfbe1bae7acfb85
HyunSu1768 <azxcv1768@gmail.com>
2024-03-10 15:19:37 +0900

    feat :: Response Status

--- src/main/kotlin/xquare/app/xquareinfra/domain/team/adapter/TeamWebAdapter.kt
+++ src/main/kotlin/xquare/app/xquareinfra/domain/team/adapter/TeamWebAdapter.kt
@@ -1,8 +1,10 @@
 package xquare.app.xquareinfra.domain.team.adapter
 
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import xquare.app.xquareinfra.domain.team.adapter.dto.request.CreateTeamRequest
 import xquare.app.xquareinfra.domain.team.application.port.`in`.CreateTeamUseCase
@@ -12,6 +14,7 @@ import xquare.app.xquareinfra.domain.team.application.port.`in`.CreateTeamUseCas
 class TeamWebAdapter(
     private val createTeamUseCase: CreateTeamUseCase
 ) {
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     fun createTeam(
         @RequestBody
