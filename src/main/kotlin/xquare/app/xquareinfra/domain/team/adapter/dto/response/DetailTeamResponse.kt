f291ffc9764b59d42c4ea3575463e08a9729718d
HyunSu1768 <azxcv1768@gmail.com>
2024-03-28 14:46:55 +0900

    FEAT :: DetailTeamResponse

---
+++ src/main/kotlin/xquare/app/xquareinfra/domain/team/adapter/dto/response/DetailTeamResponse.kt
@@ -0,0 +1,12 @@
+package xquare.app.xquareinfra.domain.team.adapter.dto.response
+
+import java.time.LocalDateTime
+
+data class DetailTeamResponse(
+    val teamNameKo: String,
+    val teamNameEn: String,
+    val memberCount: Int,
+    val adminName: String,
+    val createdAt: LocalDateTime,
+    val memberList: List<TeamMemberResponse>
+)
