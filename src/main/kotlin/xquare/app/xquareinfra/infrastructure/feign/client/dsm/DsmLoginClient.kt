2caa8904c6b4152ec2237387316ce5e689e24297
HyunSu1768 <azxcv1768@gmail.com>
2024-03-28 14:50:50 +0900

    FEAT :: DsmLoginClient

---
+++ src/main/kotlin/xquare/app/xquareinfra/infrastructure/feign/client/dsm/DsmLoginClient.kt
@@ -0,0 +1,12 @@
+package xquare.app.xquareinfra.infrastructure.feign.client.dsm
+
+import org.springframework.cloud.openfeign.FeignClient
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import xquare.app.xquareinfra.infrastructure.feign.client.dsm.dto.GetDsmUserInfoResponse
+
+@FeignClient(name = "dsm-login", url = "https://prod-server.xquare.app/dsm-login")
+interface DsmLoginClient {
+    @GetMapping("/user/user-data")
+    fun getUserInfo(@RequestParam("account_id") accountId: String, @RequestParam("password") password: String): GetDsmUserInfoResponse
+}
