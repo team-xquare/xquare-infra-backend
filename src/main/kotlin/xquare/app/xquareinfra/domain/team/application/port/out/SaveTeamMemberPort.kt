def401e529ae4343cc859a820167f24c753c9973
HyunSu1768 <azxcv1768@gmail.com>
2024-03-28 14:48:48 +0900

    BURN :: SaveTeamMemberPort

--- src/main/kotlin/xquare/app/xquareinfra/domain/team/application/port/out/SaveTeamMemberPort.kt
+++
@@ -1,7 +0,0 @@
-package xquare.app.xquareinfra.domain.team.application.port.out
-
-import xquare.app.xquareinfra.domain.team.domain.TeamMember
-
-interface SaveTeamMemberPort {
-    fun save(teamMember: TeamMember): TeamMember
-}
