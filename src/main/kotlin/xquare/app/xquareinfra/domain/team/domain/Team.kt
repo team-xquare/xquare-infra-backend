a9151cf7d127c27f3ba0db1eac169c181bb582c4
HyunSu1768 <azxcv1768@gmail.com>
2024-03-28 14:49:51 +0900

    FEAT :: Team

--- src/main/kotlin/xquare/app/xquareinfra/domain/team/domain/Team.kt
+++ src/main/kotlin/xquare/app/xquareinfra/domain/team/domain/Team.kt
@@ -2,16 +2,22 @@ package xquare.app.xquareinfra.domain.team.domain
 
 import xquare.app.xquareinfra.domain.BaseUUIDEntity
 import xquare.app.xquareinfra.domain.team.domain.type.TeamType
+import java.time.LocalDateTime
 import java.util.*
 import javax.persistence.*
 
 @Entity(name = "tbl_team")
 class Team(
     id: UUID?,
+    adminId: UUID,
     teamNameKo: String,
     teamNameEn: String,
     teamType: TeamType,
 ) : BaseUUIDEntity(id) {
+    @Column(name = "admin_name", nullable = false, columnDefinition = "BINARY(16)")
+    var adminId: UUID = adminId
+        protected set
+
     @Column(name = "team_name_ko", nullable = false)
     var teamNameKo: String = teamNameKo
         protected set
@@ -25,7 +31,9 @@ class Team(
     var teamType: TeamType = teamType
         protected set
 
-    @OneToMany(mappedBy = "team", cascade = [CascadeType.REMOVE])
-    var teamMemberList: MutableList<TeamMember> = arrayListOf()
-        protected set
+    @OneToMany(mappedBy = "team", cascade = [CascadeType.ALL], orphanRemoval = true, fetch = FetchType.LAZY)
+    val members: MutableSet<UserTeam> = HashSet()
+
+    @Column(name = "created_at", nullable = false)
+    val createdAt: LocalDateTime? = LocalDateTime.now()
 }
