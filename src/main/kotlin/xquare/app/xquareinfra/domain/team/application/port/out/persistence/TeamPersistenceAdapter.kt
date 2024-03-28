2dc32fd21f9b2df262ad1ce703396079a5a17016
HyunSu1768 <azxcv1768@gmail.com>
2024-03-28 14:48:31 +0900

    FEAT :: TeamPersistenceAdapter

--- src/main/kotlin/xquare/app/xquareinfra/domain/team/application/port/out/persistence/TeamPersistenceAdapter.kt
+++ src/main/kotlin/xquare/app/xquareinfra/domain/team/application/port/out/persistence/TeamPersistenceAdapter.kt
@@ -1,15 +1,19 @@
 package xquare.app.xquareinfra.domain.team.application.port.out.persistence
 
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Component
 import xquare.app.xquareinfra.domain.team.application.port.out.ExistsTeamPort
+import xquare.app.xquareinfra.domain.team.application.port.out.FindTeamPort
 import xquare.app.xquareinfra.domain.team.application.port.out.SaveTeamPort
 import xquare.app.xquareinfra.domain.team.application.port.out.persistence.repository.TeamRepository
 import xquare.app.xquareinfra.domain.team.domain.Team
+import java.util.*
 
 @Component
 class TeamPersistenceAdapter(
     private val teamRepository: TeamRepository
-): SaveTeamPort, ExistsTeamPort {
+): SaveTeamPort, ExistsTeamPort, FindTeamPort {
     override fun save(team: Team): Team = teamRepository.save(team)
     override fun existsByTeamNameEn(teamNameEn: String): Boolean = teamRepository.existsByTeamNameEn(teamNameEn)
+    override fun findById(teamId: UUID): Team? = teamRepository.findByIdOrNull(teamId)
 }
