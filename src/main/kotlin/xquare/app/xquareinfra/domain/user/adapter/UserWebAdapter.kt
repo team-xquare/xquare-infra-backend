96c87fdfa1fd6c031a91f6cab41f13358211b3ed
HyunSu1768 <azxcv1768@gmail.com>
2024-03-28 14:50:03 +0900

    FEAT :: UserWebAdapter

---
+++ src/main/kotlin/xquare/app/xquareinfra/domain/user/adapter/UserWebAdapter.kt
@@ -0,0 +1,11 @@
+package xquare.app.xquareinfra.domain.user.adapter
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/user")
+class UserWebAdapter(
+) {
+
+}
