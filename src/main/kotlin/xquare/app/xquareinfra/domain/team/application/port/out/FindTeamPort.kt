0f811c14d78896b06f2bd1619ea3d4c53bc41347
HyunSu1768 <azxcv1768@gmail.com>
2024-03-28 14:47:55 +0900

    FEAT :: FindTeamPort

---
+++ src/main/kotlin/xquare/app/xquareinfra/domain/team/application/port/out/FindTeamPort.kt
@@ -0,0 +1,8 @@
+package xquare.app.xquareinfra.domain.team.application.port.out
+
+import xquare.app.xquareinfra.domain.team.domain.Team
+import java.util.UUID
+
+interface FindTeamPort {
+    fun findById(teamId: UUID): Team?
+}
