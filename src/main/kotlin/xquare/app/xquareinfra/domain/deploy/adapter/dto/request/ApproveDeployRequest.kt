a9ec7bbd9619f0d009fff3244deee7e803fd00f5
HyunSu1768 <azxcv1768@gmail.com>
2024-03-29 21:07:54 +0900

    BUG :: 카멜케이스 설정

--- src/main/kotlin/xquare/app/xquareinfra/domain/deploy/adapter/dto/request/ApproveDeployRequest.kt
+++ src/main/kotlin/xquare/app/xquareinfra/domain/deploy/adapter/dto/request/ApproveDeployRequest.kt
@@ -1,5 +1,9 @@
 package xquare.app.xquareinfra.domain.deploy.adapter.dto.request
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import com.fasterxml.jackson.databind.annotation.JsonNaming
+
+@JsonNaming(PropertyNamingStrategies.LowerCamelCaseStrategy::class)
 data class ApproveDeployRequest(
     val accessKey: String,
     val secretKey: String
