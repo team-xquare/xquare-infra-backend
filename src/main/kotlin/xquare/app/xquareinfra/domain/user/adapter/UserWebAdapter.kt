86ae284b0a57d470f67ee792dd1989379ca5fd35
HyunSu1768 <azxcv1768@gmail.com>
2024-05-07 21:56:34 +0900

    feat :: 유저 전체 조회

--- src/main/kotlin/xquare/app/xquareinfra/domain/user/adapter/UserWebAdapter.kt
+++ src/main/kotlin/xquare/app/xquareinfra/domain/user/adapter/UserWebAdapter.kt
@@ -1,11 +1,16 @@
 package xquare.app.xquareinfra.domain.user.adapter
 
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import xquare.app.xquareinfra.domain.user.adapter.dto.response.UserSearchResponse
+import xquare.app.xquareinfra.domain.user.application.port.`in`.FindAllSearchUserUseCase
 
 @RestController
 @RequestMapping("/user")
 class UserWebAdapter(
+    private val findAllSearchUserUseCase: FindAllSearchUserUseCase
 ) {
-
+    @GetMapping("/all")
+    fun findAllUser(): List<UserSearchResponse> = findAllSearchUserUseCase.findAllSearchUser()
 }
