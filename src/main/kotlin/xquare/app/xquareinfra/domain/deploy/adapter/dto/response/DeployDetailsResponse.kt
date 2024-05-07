92883b1e4a8ce20cc565332ac4b70e6df6ec650a
HyunSu1768 <azxcv1768@gmail.com>
2024-05-07 23:22:22 +0900

    feat :: response 추가

--- src/main/kotlin/xquare/app/xquareinfra/domain/deploy/adapter/dto/response/DeployDetailsResponse.kt
+++ src/main/kotlin/xquare/app/xquareinfra/domain/deploy/adapter/dto/response/DeployDetailsResponse.kt
@@ -3,6 +3,7 @@ package xquare.app.xquareinfra.domain.deploy.adapter.dto.response
 import xquare.app.xquareinfra.domain.deploy.domain.DeployStatus
 
 data class DeployDetailsResponse(
+    val deployName: String,
     val teamNameEn: String,
     val teamNameKo: String,
     val oneLineDescription: String,
