eb8ff2b9146afc360504738581e00c83936fd63b
HyunSu1768 <azxcv1768@gmail.com>
2024-05-07 22:43:14 +0900

    feat :: 배포 아이디 추가

--- src/main/kotlin/xquare/app/xquareinfra/domain/deploy/adapter/dto/response/SimpleDeployResponse.kt
+++ src/main/kotlin/xquare/app/xquareinfra/domain/deploy/adapter/dto/response/SimpleDeployResponse.kt
@@ -1,8 +1,10 @@
 package xquare.app.xquareinfra.domain.deploy.adapter.dto.response
 
 import xquare.app.xquareinfra.domain.deploy.domain.DeployStatus
+import java.util.UUID
 
 data class SimpleDeployResponse(
+    val deployId: UUID,
     val deployName: String,
     val repository: String,
     val deployStatus: DeployStatus
