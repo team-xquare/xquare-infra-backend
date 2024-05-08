d11540ea0107ea3f2229449d76201e5afa3bac3d
HyunSu1768 <azxcv1768@gmail.com>
2024-05-08 17:13:59 +0900

    feat :: 팀생성시 유저 추가 누락

--- src/main/kotlin/xquare/app/xquareinfra/domain/team/application/service/CreateTeamService.kt
+++ src/main/kotlin/xquare/app/xquareinfra/domain/team/application/service/CreateTeamService.kt
@@ -6,9 +6,11 @@ import xquare.app.xquareinfra.domain.auth.application.port.out.ReadCurrentUserPo
 import xquare.app.xquareinfra.domain.team.adapter.dto.request.CreateTeamRequest
 import xquare.app.xquareinfra.domain.team.application.port.`in`.CreateTeamUseCase
 import xquare.app.xquareinfra.domain.team.application.port.out.ExistsTeamPort
+import xquare.app.xquareinfra.domain.team.application.port.out.ExistsUserTeamPort
 import xquare.app.xquareinfra.domain.team.application.port.out.SaveTeamPort
 import xquare.app.xquareinfra.domain.team.domain.Team
 import xquare.app.xquareinfra.domain.team.domain.role.TeamMemberRole
+import xquare.app.xquareinfra.domain.user.application.port.out.FindUserPort
 import xquare.app.xquareinfra.infrastructure.exception.BusinessLogicException
 
 @Transactional
@@ -16,7 +18,9 @@ import xquare.app.xquareinfra.infrastructure.exception.BusinessLogicException
 class CreateTeamService(
     private val saveTeamPort: SaveTeamPort,
     private val existsTeamPort: ExistsTeamPort,
-    private val readCurrentUserPort: ReadCurrentUserPort
+    private val readCurrentUserPort: ReadCurrentUserPort,
+    private val findUserPort: FindUserPort,
+    private val existsUserTeamPort: ExistsUserTeamPort
 ): CreateTeamUseCase {
     override fun create(req: CreateTeamRequest) {
         if(existsTeamPort.existsByTeamNameEn(req.teamNameEn)) {
@@ -35,5 +39,12 @@ class CreateTeamService(
             )
         )
         user.addTeam(team, TeamMemberRole.ADMINISTRATOR)
+        req.teamMemberList.map {
+            val addMember = findUserPort.findById(it) ?: throw BusinessLogicException.USER_NOT_FOUND
+            if(existsUserTeamPort.existsByTeamAndUser(team, addMember)) {
+                throw BusinessLogicException.ALREADY_EXISTS_USER_TEAM
+            }
+            addMember.addTeam(team, TeamMemberRole.MEMBER)
+        }
     }
 }
