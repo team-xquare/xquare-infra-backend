df3a922e2009b76f2045bd34a668f7e71ae97d29
HyunSu1768 <azxcv1768@gmail.com>
2024-05-14 23:09:20 +0900

    bug :: 버그 수정

--- src/main/kotlin/xquare/app/xquareinfra/domain/container/util/ContainerUtil.kt
+++ src/main/kotlin/xquare/app/xquareinfra/domain/container/util/ContainerUtil.kt
@@ -5,7 +5,7 @@ import xquare.app.xquareinfra.domain.container.domain.ContainerEnvironment
 
 object ContainerUtil {
     fun generateDomain(container: Container): String {
-        if(container.subDomain!!.isEmpty()) {
+        if(container.subDomain!!.isEmpty() || container.subDomain!! == "null" || container.subDomain == null) {
             val baseDomain = when (container.containerEnvironment) {
                 ContainerEnvironment.prod -> "prod-server.xquare.app"
                 else -> "stag-server.xquare.app"
