6c5ef9723168ee006a52cd17c3ff481f846bccef
HyunSu1768 <azxcv1768@gmail.com>
2024-07-08 14:04:15 +0900

    bug :: 버그수정

--- src/main/kotlin/xquare/app/xquareinfra/infrastructure/feign/client/dsm/DsmLoginClient.kt
+++ src/main/kotlin/xquare/app/xquareinfra/infrastructure/feign/client/dsm/DsmLoginClient.kt
@@ -6,17 +6,18 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy
 import com.fasterxml.jackson.databind.annotation.JsonNaming
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestParam
 import xquare.app.xquareinfra.infrastructure.feign.client.dsm.dto.GetDsmUserInfoResponse
 
 @FeignClient(name = "dsm-login", url = "https://prod-server.xquare.app/dsm-login")
 interface DsmLoginClient {
-    @GetMapping("/user/user-data")
+    @PostMapping("/user/user-data")
     fun getUserInfo(@RequestBody dsmLoginRequest: DsmLoginRequest): GetDsmUserInfoResponse
 }
 
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
+@JsonNaming(SnakeCaseStrategy::class)
 data class DsmLoginRequest(
     val accountId: String,
     val password: String
