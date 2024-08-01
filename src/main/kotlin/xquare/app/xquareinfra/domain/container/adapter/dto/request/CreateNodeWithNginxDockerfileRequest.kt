af749a776a74b0157083e7ba547d96d48f54e650
HyunSu1768 <azxcv1768@gmail.com>
2024-08-01 23:08:49 +0900

    feat :: dto 수정

--- src/main/kotlin/xquare/app/xquareinfra/domain/container/adapter/dto/request/CreateNodeWithNginxDockerfileRequest.kt
+++ src/main/kotlin/xquare/app/xquareinfra/domain/container/adapter/dto/request/CreateNodeWithNginxDockerfileRequest.kt
@@ -2,7 +2,7 @@ package xquare.app.xquareinfra.domain.container.adapter.dto.request
 
 data class CreateNodeWithNginxDockerfileRequest(
     val nodeVersion: Int,
-    val commands: List<String> = emptyList(),
+    val buildCommands: List<String> = emptyList(),
     val builder: String = "node_with_nginx",
     val outputDir: String,
     var port: Int?
