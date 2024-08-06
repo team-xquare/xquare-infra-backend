1f87d915e204e67a4eea0fb3c4dab90afc3cb070
HyunSu1768 <azxcv1768@gmail.com>
2024-08-06 09:02:34 +0900

    feat :: 환경변수 수정 시 재배포

--- src/main/kotlin/xquare/app/xquareinfra/infrastructure/external/feign/client/gocd/GocdClient.kt
+++ src/main/kotlin/xquare/app/xquareinfra/infrastructure/external/feign/client/gocd/GocdClient.kt
@@ -3,8 +3,11 @@ package xquare.app.xquareinfra.infrastructure.external.client.gocd
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
 import xquare.app.xquareinfra.infrastructure.external.client.gocd.dto.response.GetPipelinesHistoryResponse
+import xquare.app.xquareinfra.infrastructure.external.feign.client.gocd.dto.request.RunSelectedJobRequest
 import xquare.app.xquareinfra.infrastructure.external.feign.config.FeignConfig
 
 @FeignClient(
@@ -26,4 +29,13 @@ interface GocdClient {
         @PathVariable("stage") stage: String,
         @RequestHeader("Accept") accept: String
     ): String
+
+    @PostMapping("/api/stages/{pipelineName}/{pipelineCounter}/{stage}/1/run-selected-jobs")
+    fun runSelectedJob(
+        @PathVariable("pipelineName") pipelineName: String,
+        @PathVariable("pipelineCounter") pipelineCounter: Int,
+        @PathVariable("stage") stage: String,
+        @RequestHeader("Accept") accept: String,
+        @RequestBody runSelectedJobRequest: RunSelectedJobRequest
+    )
 }
