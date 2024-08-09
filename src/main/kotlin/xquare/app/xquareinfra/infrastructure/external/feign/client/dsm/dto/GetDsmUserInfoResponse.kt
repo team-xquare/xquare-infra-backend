3d90e175fe0026329ead35f3a236dcc22f2570a5
HyunSu1768 <azxcv1768@gmail.com>
2024-08-09 19:58:33 +0900

    bug :: 버그픽스

--- src/main/kotlin/xquare/app/xquareinfra/infrastructure/external/feign/client/dsm/dto/GetDsmUserInfoResponse.kt
+++ src/main/kotlin/xquare/app/xquareinfra/infrastructure/external/feign/client/dsm/dto/GetDsmUserInfoResponse.kt
@@ -1,9 +1,12 @@
-package xquare.app.xquareinfra.infrastructure.external.client.dsm.dto
+package xquare.app.xquareinfra.infrastructure.external.feign.client.dsm.dto
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.databind.annotation.JsonNaming
+import xquare.app.xquareinfra.infrastructure.external.client.dsm.dto.DsmUserRole
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
 data class GetDsmUserInfoResponse(
     val id: String,
@@ -18,5 +21,5 @@ data class GetDsmUserInfoResponse(
     @JsonProperty("profileImgUrl")
     val profileImgUrl: String,
     @JsonProperty("clubName")
-    val clubName: String
+    val clubName: String? = null
 )
