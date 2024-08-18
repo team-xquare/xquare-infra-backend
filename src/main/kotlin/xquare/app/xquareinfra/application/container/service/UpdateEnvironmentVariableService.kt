e5dcde97a99575a69816c06550e0c551b51e613d
HyunSu1768 <azxcv1768@gmail.com>
2024-08-19 08:47:15 +0900

    Refactor KubernetesClientUtil to KubernetesOperationService

--- src/main/kotlin/xquare/app/xquareinfra/application/container/service/UpdateEnvironmentVariableService.kt
+++ src/main/kotlin/xquare/app/xquareinfra/application/container/service/UpdateEnvironmentVariableService.kt
@@ -3,16 +3,13 @@ package xquare.app.xquareinfra.application.container.service
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import xquare.app.xquareinfra.application.auth.port.out.ReadCurrentUserPort
-import xquare.app.xquareinfra.application.container.port.`in`.UpdateEnvironmentVariableUseCase
-import xquare.app.xquareinfra.application.container.port.out.FindContainerPort
 import xquare.app.xquareinfra.domain.container.domain.ContainerEnvironment
 import xquare.app.xquareinfra.application.deploy.port.out.FindDeployPort
-import xquare.app.xquareinfra.application.team.port.out.ExistsUserTeamPort
 import xquare.app.xquareinfra.infrastructure.exception.BusinessLogicException
 import xquare.app.xquareinfra.infrastructure.exception.XquareException
 import xquare.app.xquareinfra.infrastructure.external.gocd.client.GocdClient
 import xquare.app.xquareinfra.infrastructure.external.gocd.client.dto.request.RunSelectedJobRequest
-import xquare.app.xquareinfra.infrastructure.kubernetes.KubernetesClientUtil
+import xquare.app.xquareinfra.infrastructure.kubernetes.KubernetesOperationService
 import xquare.app.xquareinfra.infrastructure.vault.VaultUtil
 import java.util.UUID
 
@@ -22,7 +19,7 @@ class UpdateEnvironmentVariableService(
     private val findDeployPort: FindDeployPort,
     private val findContainerPort: xquare.app.xquareinfra.application.container.port.out.FindContainerPort,
     private val vaultUtil: VaultUtil,
-    private val kubernetesClientUtil: KubernetesClientUtil,
+    private val kubernetesOperationService: KubernetesOperationService,
     private val readCurrentUserPort: ReadCurrentUserPort,
     private val existsUserTeamPort: xquare.app.xquareinfra.application.team.port.out.ExistsUserTeamPort,
     private val gocdClient: GocdClient
@@ -48,7 +45,7 @@ class UpdateEnvironmentVariableService(
         vaultUtil.addSecret(environmentVariable, path)
 
         val namespace = "${deploy.team.teamNameEn}-${container.containerEnvironment.name}"
-        kubernetesClientUtil.deleteSecret(namespace, path)
+        kubernetesOperationService.deleteSecret(namespace, path)
 
         if(deploy.isV2) {
             val pipelineName = "build-${deploy.deployName}-${container.containerEnvironment.name}"
