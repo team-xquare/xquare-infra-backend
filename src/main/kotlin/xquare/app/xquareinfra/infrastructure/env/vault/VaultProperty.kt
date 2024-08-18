03bf635431b0de87a58ba3ac1c8ffe5815d53418
HyunSu1768 <azxcv1768@gmail.com>
2024-08-19 08:43:27 +0900

    Refactor VaultProperty package structure

--- src/main/kotlin/xquare/app/xquareinfra/infrastructure/env/vault/VaultProperty.kt
+++ src/main/kotlin/xquare/app/xquareinfra/infrastructure/env/vault/VaultProperty.kt
@@ -1,4 +1,4 @@
-package xquare.app.xquareinfra.infrastructure.vault.env
+package xquare.app.xquareinfra.infrastructure.env.vault
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
