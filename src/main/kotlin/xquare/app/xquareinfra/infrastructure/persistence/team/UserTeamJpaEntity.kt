aecc2c868db7defacb34bd5ec66bd1448dbc4d35
HyunSu1768 <azxcv1768@gmail.com>
2024-08-19 23:27:14 +0900

    Add @Table annotation to UserTeamJpaEntity

--- src/main/kotlin/xquare/app/xquareinfra/infrastructure/persistence/team/UserTeamJpaEntity.kt
+++ src/main/kotlin/xquare/app/xquareinfra/infrastructure/persistence/team/UserTeamJpaEntity.kt
@@ -4,6 +4,7 @@ import xquare.app.xquareinfra.domain.team.model.role.TeamMemberRole
 import xquare.app.xquareinfra.infrastructure.persistence.user.UserJpaEntity
 import javax.persistence.*
 
+@Table(name = "user_team")
 @Entity(name = "user_team")
 class UserTeamJpaEntity(
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
