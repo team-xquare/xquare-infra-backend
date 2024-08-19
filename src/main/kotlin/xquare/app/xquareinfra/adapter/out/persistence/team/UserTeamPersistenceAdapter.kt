076983e389f42e1837bb70eacd30846dd7e5127d
HyunSu1768 <azxcv1768@gmail.com>
2024-08-19 11:24:33 +0900

    Simplify import statements in adapter

--- src/main/kotlin/xquare/app/xquareinfra/adapter/out/persistence/team/UserTeamPersistenceAdapter.kt
+++ src/main/kotlin/xquare/app/xquareinfra/adapter/out/persistence/team/UserTeamPersistenceAdapter.kt
@@ -2,6 +2,8 @@ package xquare.app.xquareinfra.adapter.out.persistence.team
 
 import org.springframework.stereotype.Component
 import xquare.app.xquareinfra.adapter.out.persistence.team.repository.UserTeamRepository
+import xquare.app.xquareinfra.application.team.port.out.ExistsUserTeamPort
+import xquare.app.xquareinfra.application.team.port.out.FindUserTeamPort
 import xquare.app.xquareinfra.infrastructure.persistence.team.TeamJpaEntity
 import xquare.app.xquareinfra.infrastructure.persistence.team.UserTeamJpaEntity
 import xquare.app.xquareinfra.infrastructure.persistence.user.UserJpaEntity
@@ -9,8 +11,7 @@ import xquare.app.xquareinfra.infrastructure.persistence.user.UserJpaEntity
 @Component
 class UserTeamPersistenceAdapter(
     private val userTeamRepository: UserTeamRepository
-): xquare.app.xquareinfra.application.team.port.out.ExistsUserTeamPort,
-    xquare.app.xquareinfra.application.team.port.out.FindUserTeamPort {
+): ExistsUserTeamPort, FindUserTeamPort {
     override fun existsByTeamAndUser(teamJpaEntity: TeamJpaEntity, userJpaEntity: UserJpaEntity): Boolean =
         userTeamRepository.existsByTeamAndUser(teamJpaEntity, userJpaEntity)
 
