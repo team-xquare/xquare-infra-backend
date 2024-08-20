feec8e154dd2606b36ab41fb222f4939e4799198
HyunSu1768 <azxcv1768@gmail.com>
2024-08-20 10:29:51 +0900

    Refactor container services into ContainerService

--- src/main/kotlin/xquare/app/xquareinfra/application/container/port/in/GetContainerByDeployIdUseCase.kt
+++
@@ -1,8 +0,0 @@
-package xquare.app.xquareinfra.application.container.port.`in`
-
-import xquare.app.xquareinfra.adapter.`in`.container.dto.response.SimpleContainerResponse
-import java.util.UUID
-
-interface GetContainerByDeployIdUseCase {
-    fun getContainerByDeploy(deployId: UUID): List<SimpleContainerResponse>
-}
