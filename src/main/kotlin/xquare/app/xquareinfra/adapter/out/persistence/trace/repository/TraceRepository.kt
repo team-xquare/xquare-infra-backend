682e3b28154a7fc8ae7130319b2378c81f7b71c4
HyunSu1768 <azxcv1768@gmail.com>
2024-08-27 22:34:53 +0900

    Implement trace retrieval by service and time range

--- src/main/kotlin/xquare/app/xquareinfra/adapter/out/persistence/trace/repository/TraceRepository.kt
+++ src/main/kotlin/xquare/app/xquareinfra/adapter/out/persistence/trace/repository/TraceRepository.kt
@@ -1,7 +1,14 @@
 package xquare.app.xquareinfra.adapter.out.persistence.trace.repository
 
 import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.data.mongodb.repository.Query
 import xquare.app.xquareinfra.infrastructure.persistence.trace.TraceMongoEntity
 
 interface TraceRepository : MongoRepository<TraceMongoEntity, String> {
+    @Query("{ 'serviceName': ?0, 'dateNano': { \$gte: ?1, \$lte: ?2 } }")
+    fun findTraceListByServiceNameInTimeRange(
+        serviceName: String,
+        startTime: Long,
+        endTime: Long
+    ): List<TraceMongoEntity>
 }
