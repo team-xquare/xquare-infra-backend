746798294e1053409eb4c0ea44a9f39f5d77852e
HyunSu1768 <azxcv1768@gmail.com>
2024-08-27 18:39:19 +0900

    Add getRootSpan endpoint in V1SpanWebAdapter

--- src/main/kotlin/xquare/app/xquareinfra/adapter/in/span/V1SpanWebAdapter.kt
+++ src/main/kotlin/xquare/app/xquareinfra/adapter/in/span/V1SpanWebAdapter.kt
@@ -1,9 +1,29 @@
 package xquare.app.xquareinfra.adapter.`in`.span
 
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import xquare.app.xquareinfra.adapter.`in`.span.dto.response.GetRootSpanListResponse
+import xquare.app.xquareinfra.application.span.port.`in`.SpanUseCase
+import xquare.app.xquareinfra.domain.container.model.ContainerEnvironment
+import java.util.UUID
 
 @RequestMapping("/span")
 @RestController
-class V1SpanWebAdapter {
+class V1SpanWebAdapter(
+    private val spanUseCase: SpanUseCase
+) {
+    @GetMapping("/root-span")
+    fun getRootSpan(
+        @RequestParam("deployId") deployId: UUID,
+        @RequestParam("environment") environment: ContainerEnvironment,
+        @RequestParam("timeRange") timeRange: Long
+    ): GetRootSpanListResponse {
+        return spanUseCase.getRootSpanByDeployIdAndEnvironment(
+            deployId = deployId,
+            environment = environment,
+            timeRangeMinute = timeRange
+        )
+    }
 }
