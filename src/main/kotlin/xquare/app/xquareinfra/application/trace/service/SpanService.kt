9bee4ae7b5b5b0ed6ca7395210e70614acd830ad
HyunSu1768 <azxcv1768@gmail.com>
2024-08-27 21:36:49 +0900

    Add tracing functionality with Span models

---
+++ src/main/kotlin/xquare/app/xquareinfra/application/trace/service/SpanService.kt
@@ -0,0 +1,48 @@
+package xquare.app.xquareinfra.application.trace.service
+
+import org.springframework.stereotype.Service
+import xquare.app.xquareinfra.adapter.`in`.trace.dto.response.GetRootSpanListResponse
+import xquare.app.xquareinfra.adapter.`in`.trace.dto.response.RootSpan
+import xquare.app.xquareinfra.application.deploy.port.out.FindDeployPort
+import xquare.app.xquareinfra.application.trace.port.`in`.SpanUseCase
+import xquare.app.xquareinfra.application.trace.port.out.FindSpanPort
+import xquare.app.xquareinfra.domain.container.model.ContainerEnvironment
+import xquare.app.xquareinfra.domain.container.util.ContainerUtil
+import xquare.app.xquareinfra.infrastructure.exception.BusinessLogicException
+import xquare.app.xquareinfra.infrastructure.util.TimeUtil
+import java.util.*
+
+@Service
+class SpanService(
+    private val findDeployPort: FindDeployPort,
+    private val findSpanPort: FindSpanPort
+) : SpanUseCase {
+    override fun getRootSpanByDeployIdAndEnvironment(
+        deployId: UUID,
+        environment: ContainerEnvironment,
+        timeRangeMinute: Long
+    ): GetRootSpanListResponse {
+        val deploy = findDeployPort.findById(deployId) ?: throw BusinessLogicException.DEPLOY_NOT_FOUND
+        val serviceName = ContainerUtil.getContainerName(deploy, environment)
+
+        val timeRangeInNanos = TimeUtil.getTimeRangeInNanos(timeRangeMinute)
+
+        val rootSpanList = findSpanPort.findRootSpanListByServiceNameInTimeRange(
+            serviceName = serviceName,
+            startTimeUnixNano = timeRangeInNanos.past,
+            endTimeUnixNano = timeRangeInNanos.now
+        )
+
+        val rootSpanDtoList = rootSpanList.map {
+            RootSpan(
+                date = TimeUtil.unixNanoToKoreanTime(it.startTimeUnixNano),
+                resource = it.name,
+                durationMs = TimeUtil.unixNanoToMilliseconds(it.endTimeUnixNano - it.startTimeUnixNano),
+                method = it.getAttributeValue("http.method")?.stringValue,
+                statusCode = it.getAttributeValue("http.status_code")?.intValue
+            )
+        }
+
+        return GetRootSpanListResponse(rootSpanDtoList)
+    }
+}
