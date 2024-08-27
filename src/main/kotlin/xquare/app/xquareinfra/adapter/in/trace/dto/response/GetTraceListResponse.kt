2eb07b7d0001b8955f60586b9700209d94ef835a
HyunSu1768 <azxcv1768@gmail.com>
2024-08-27 22:13:16 +0900

    Refactor trace handling and rename classes

--- src/main/kotlin/xquare/app/xquareinfra/adapter/in/trace/dto/response/GetTraceListResponse.kt
+++ src/main/kotlin/xquare/app/xquareinfra/adapter/in/trace/dto/response/GetTraceListResponse.kt
@@ -1,12 +1,12 @@
 package xquare.app.xquareinfra.adapter.`in`.trace.dto.response
 
-data class GetRootSpanListResponse(
-    val spans: List<RootSpan>
+data class GetTraceListResponse(
+    val traces: List<Trace>
 )
 
-data class RootSpan(
-    val date: String,
-    val resource: String,
+data class Trace(
+    val dateNano: Long,
+    val resource: String?,
     val durationMs: Long,
     val method: String?,
     val statusCode: Long?
--- src/main/kotlin/xquare/app/xquareinfra/application/trace/service/TraceService.kt
+++ src/main/kotlin/xquare/app/xquareinfra/application/trace/service/TraceService.kt
@@ -1,11 +1,11 @@
 package xquare.app.xquareinfra.application.trace.service
 
 import org.springframework.stereotype.Service
-import xquare.app.xquareinfra.adapter.`in`.trace.dto.response.GetRootSpanListResponse
-import xquare.app.xquareinfra.adapter.`in`.trace.dto.response.RootSpan
+import xquare.app.xquareinfra.adapter.`in`.trace.dto.response.GetTraceListResponse
+import xquare.app.xquareinfra.adapter.`in`.trace.dto.response.Trace
 import xquare.app.xquareinfra.application.deploy.port.out.FindDeployPort
-import xquare.app.xquareinfra.application.trace.port.`in`.SpanUseCase
-import xquare.app.xquareinfra.application.trace.port.out.FindSpanPort
+import xquare.app.xquareinfra.application.trace.port.`in`.TraceUseCase
+import xquare.app.xquareinfra.application.trace.port.out.FindTracePort
 import xquare.app.xquareinfra.domain.container.model.ContainerEnvironment
 import xquare.app.xquareinfra.domain.container.util.ContainerUtil
 import xquare.app.xquareinfra.infrastructure.exception.BusinessLogicException
@@ -13,36 +13,37 @@ import xquare.app.xquareinfra.infrastructure.util.TimeUtil
 import java.util.*
 
 @Service
-class SpanService(
+class TraceService(
     private val findDeployPort: FindDeployPort,
-    private val findSpanPort: FindSpanPort
-) : SpanUseCase {
+    private val findTracePort: FindTracePort
+) : TraceUseCase {
     override fun getRootSpanByDeployIdAndEnvironment(
         deployId: UUID,
         environment: ContainerEnvironment,
         timeRangeMinute: Long
-    ): GetRootSpanListResponse {
+    ): GetTraceListResponse {
         val deploy = findDeployPort.findById(deployId) ?: throw BusinessLogicException.DEPLOY_NOT_FOUND
         val serviceName = ContainerUtil.getContainerName(deploy, environment)
 
         val timeRangeInNanos = TimeUtil.getTimeRangeInNanos(timeRangeMinute)
 
-        val rootSpanList = findSpanPort.findRootSpanListByServiceNameInTimeRange(
+        val rootSpanList = findTracePort.findTraceListByServiceNameInTimeRange(
             serviceName = serviceName,
             startTimeUnixNano = timeRangeInNanos.past,
             endTimeUnixNano = timeRangeInNanos.now
         )
 
-        val rootSpanDtoList = rootSpanList.map {
-            RootSpan(
-                date = TimeUtil.unixNanoToKoreanTime(it.startTimeUnixNano),
-                resource = it.name,
-                durationMs = TimeUtil.unixNanoToMilliseconds(it.endTimeUnixNano - it.startTimeUnixNano),
-                method = it.getAttributeValue("http.method")?.stringValue,
-                statusCode = it.getAttributeValue("http.status_code")?.intValue
+        val traceDtoList = rootSpanList.map {
+            val rootSpan = it.findRootSpan()
+            Trace(
+                dateNano = it.dateNano,
+                resource = rootSpan?.name,
+                durationMs = TimeUtil.unixNanoToMilliseconds(it.durationNano),
+                method = rootSpan?.getAttributeValue("http.method")?.stringValue,
+                statusCode = rootSpan?.getAttributeValue("http.status_code")?.intValue
             )
         }
 
-        return GetRootSpanListResponse(rootSpanDtoList)
+        return GetTraceListResponse(traceDtoList)
     }
 }
