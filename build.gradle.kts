d45ff704567c1e63911fc31d8282e3619e67ce11
HyunSu1768 <azxcv1768@gmail.com>
2024-08-27 13:53:21 +0900

    Add Spring Boot MongoDB starter dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -85,6 +85,7 @@ dependencies {
 
     implementation("org.mapstruct:mapstruct:1.5.3.Final")
     kapt("org.mapstruct:mapstruct-processor:1.5.3.Final")
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
 }
 
 dependencyManagement {
