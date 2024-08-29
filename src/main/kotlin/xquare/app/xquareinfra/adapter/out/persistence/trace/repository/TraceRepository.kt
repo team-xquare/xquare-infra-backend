926b44b726b0dd93579d6cad2824ecd68803926d
HyunSu1768 <azxcv1768@gmail.com>
2024-08-29 11:29:45 +0900

    Rename TraceRepository to TraceMongoEntityRepository

--- src/main/kotlin/xquare/app/xquareinfra/adapter/out/persistence/trace/repository/TraceRepository.kt
+++ src/main/kotlin/xquare/app/xquareinfra/adapter/out/persistence/trace/repository/TraceRepository.kt
@@ -1,11 +1,12 @@
 package xquare.app.xquareinfra.adapter.out.persistence.trace.repository
 
 import org.springframework.data.mongodb.repository.MongoRepository
-import org.springframework.data.mongodb.repository.Query
-import xquare.app.xquareinfra.domain.trace.model.Trace
 import xquare.app.xquareinfra.infrastructure.persistence.trace.TraceMongoEntity
 
-interface TraceRepository : MongoRepository<TraceMongoEntity, String> {
-    @Query("{ 'serviceName': ?0, 'dateNano': { \$gte: ?1, \$lte: ?2 } }")
-    fun findAllByServiceNameInTimeRange(serviceName: String, startTimeUnix: Long, endTimeUnix: Long): List<TraceMongoEntity>
+interface TraceMongoEntityRepository : MongoRepository<TraceMongoEntity, String> {
+    fun findAllByServiceNameAndDateNanoBetween(
+        serviceName: String?,
+        startTimeUnix: Long,
+        endTimeUnix: Long
+    ): List<TraceMongoEntity>
 }
