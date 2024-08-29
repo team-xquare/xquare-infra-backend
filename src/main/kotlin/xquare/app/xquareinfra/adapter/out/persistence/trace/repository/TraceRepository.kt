4becdc3a6a6af2377daebb4462d0bf9b8f073fe4
HyunSu1768 <azxcv1768@gmail.com>
2024-08-29 12:42:20 +0900

    Refactor trace repository to use MongoTemplate

--- src/main/kotlin/xquare/app/xquareinfra/adapter/out/persistence/trace/repository/TraceRepository.kt
+++ src/main/kotlin/xquare/app/xquareinfra/adapter/out/persistence/trace/repository/TraceRepository.kt
@@ -1,12 +1,27 @@
 package xquare.app.xquareinfra.adapter.out.persistence.trace.repository
 
-import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.data.mongodb.core.MongoTemplate
 import xquare.app.xquareinfra.infrastructure.persistence.trace.TraceMongoEntity
+import org.springframework.data.mongodb.core.query.Criteria
+import org.springframework.data.mongodb.core.query.Query
+import org.springframework.stereotype.Repository
 
-interface TraceMongoEntityRepository : MongoRepository<TraceMongoEntity, String> {
-    fun findAllByServiceNameAndDateNanoBetween(
-        serviceName: String?,
+@Repository
+class TraceMongoEntityRepository(
+    private val mongoTemplate: MongoTemplate
+) {
+    fun findByServiceNameAndDateNanoBetween(
+        serviceName: String,
         startTimeUnix: Long,
         endTimeUnix: Long
-    ): List<TraceMongoEntity>
+    ): List<TraceMongoEntity> {
+        val query = Query()
+
+        query.addCriteria(
+            Criteria.where("serviceName").`is`(serviceName)
+                .and("dateNano").gte(startTimeUnix).lte(endTimeUnix)
+        )
+
+        return mongoTemplate.find(query, TraceMongoEntity::class.java)
+    }
 }
