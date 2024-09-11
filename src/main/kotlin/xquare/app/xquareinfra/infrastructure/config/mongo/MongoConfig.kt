99766ff29c98e298512aa7694827f88714a4d316
HyunSu1768 <azxcv1768@gmail.com>
2024-09-11 16:50:51 +0900

    feat(mongoConfig.kt): implement SSL configuration for MongoDB connection to enhance security chore(mongoConfig.kt): add methods to download and manage SSL certificates for secure MongoDB connections

--- src/main/kotlin/xquare/app/xquareinfra/infrastructure/config/mongo/MongoConfig.kt
+++ src/main/kotlin/xquare/app/xquareinfra/infrastructure/config/mongo/MongoConfig.kt
@@ -9,9 +9,17 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration
 import org.springframework.data.mongodb.core.MongoTemplate
+import java.io.ByteArrayInputStream
+import java.net.URL
+import java.security.KeyStore
+import java.security.cert.CertificateFactory
+import java.security.cert.X509Certificate
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManagerFactory
 
 @Configuration
 class MongoConfig : AbstractMongoClientConfiguration() {
+
     @Value("\${spring.data.mongodb.uri}")
     private lateinit var mongoUri: String
 
@@ -19,11 +27,41 @@ class MongoConfig : AbstractMongoClientConfiguration() {
 
     @Bean
     override fun mongoClient(): MongoClient {
+        val sslContext = createSSLContext()
+
         val connectionString = ConnectionString(mongoUri)
         val mongoClientSettings = MongoClientSettings.builder()
             .applyConnectionString(connectionString)
+            .applyToSslSettings { builder ->
+                builder.enabled(true)
+                    .context(sslContext)
+            }
             .build()
+
         return MongoClients.create(mongoClientSettings)
+    }
+
+    private fun createSSLContext(): SSLContext {
+        val certUrl = "https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem"
+        val cert = downloadCertificate(certUrl)
+
+        val keyStore = KeyStore.getInstance(KeyStore.getDefaultType())
+        keyStore.load(null, null)
+        keyStore.setCertificateEntry("docdb-root", cert)
+
+        val tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm())
+        tmf.init(keyStore)
+
+        val sslContext = SSLContext.getInstance("TLS")
+        sslContext.init(null, tmf.trustManagers, null)
+
+        return sslContext
+    }
+
+    private fun downloadCertificate(url: String): X509Certificate {
+        val certBytes = URL(url).readBytes()
+        val cf = CertificateFactory.getInstance("X.509")
+        return cf.generateCertificate(ByteArrayInputStream(certBytes)) as X509Certificate
     }
 
     @Bean
