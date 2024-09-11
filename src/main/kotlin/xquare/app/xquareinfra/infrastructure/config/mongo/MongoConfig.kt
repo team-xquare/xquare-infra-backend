6c39f64fbe30f9019c4d4886610d2a385f73bfca
HyunSu1768 <azxcv1768@gmail.com>
2024-09-11 16:40:43 +0900

    feat(mongo): implement MongoDB configuration using Spring Data MongoDB
    Add a new MongoConfig class that extends AbstractMongoClientConfiguration
    to configure MongoDB connection settings. This change allows for
    dynamic configuration of the MongoDB URI and provides a MongoTemplate
    bean for easier database operations.

--- src/main/kotlin/xquare/app/xquareinfra/infrastructure/config/mongo/MongoConfig.kt
+++ src/main/kotlin/xquare/app/xquareinfra/infrastructure/config/mongo/MongoConfig.kt
@@ -1,4 +1,31 @@
 package xquare.app.xquareinfra.infrastructure.config.mongo
 
-class MongoConfig {
+import com.mongodb.ConnectionString
+import com.mongodb.MongoClientSettings
+import com.mongodb.client.MongoClient
+import com.mongodb.client.MongoClients
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration
+import org.springframework.data.mongodb.core.MongoTemplate
+
+@Configuration
+class MongoConfig : AbstractMongoClientConfiguration() {
+    @Value("\${spring.data.mongodb.uri}")
+    private lateinit var mongoUri: String
+
+    override fun getDatabaseName(): String = "tracing"
+
+    @Bean
+    override fun mongoClient(): MongoClient {
+        val connectionString = ConnectionString(mongoUri)
+        val mongoClientSettings = MongoClientSettings.builder()
+            .applyConnectionString(connectionString)
+            .build()
+        return MongoClients.create(mongoClientSettings)
+    }
+
+    @Bean
+    fun mongoTemplate(): MongoTemplate = MongoTemplate(mongoClient(), databaseName)
 }
