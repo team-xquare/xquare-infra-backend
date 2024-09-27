1cfcc4e9bf60e91aad08eebd4cda4525fe1108c4
HyunSu1768 <azxcv1768@gmail.com>
2024-09-27 09:59:21 +0900

    feat(ContainerPipelinePort): rename interface and add schedulePipeline method to enhance container pipeline management functionality

--- src/main/kotlin/xquare/app/xquareinfra/application/container/port/out/ContainerPipelinePort.kt
+++ src/main/kotlin/xquare/app/xquareinfra/application/container/port/out/ContainerPipelinePort.kt
@@ -3,6 +3,14 @@ package xquare.app.xquareinfra.application.container.port.out
 import xquare.app.xquareinfra.adapter.`in`.container.dto.response.DeployHistoryResponse
 import xquare.app.xquareinfra.domain.container.model.ContainerEnvironment
 
-interface ContainerDeployHistoryPort {
-    fun getContainerDeployHistory(deployName: String, containerEnvironment: ContainerEnvironment): List<DeployHistoryResponse>
+interface ContainerPipelinePort {
+    fun getContainerPipelineHistory(
+        deployName: String,
+        containerEnvironment: ContainerEnvironment
+    ): List<DeployHistoryResponse>
+
+    fun schedulePipeline(
+        deployName: String,
+        containerEnvironment: ContainerEnvironment
+    )
 }
