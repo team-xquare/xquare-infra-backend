8c5936ffe4505cdc589b62a7766f2fc1fe2073cc
HyunSu1768 <azxcv1768@gmail.com>
2024-10-05 21:54:23 +0900

    fix(CreateNodeWithNginxDockerfileRequest.kt): change nodeVersion type from Int to String to accommodate version formats

--- src/main/kotlin/xquare/app/xquareinfra/adapter/in/container/dto/request/CreateNodeWithNginxDockerfileRequest.kt
+++ src/main/kotlin/xquare/app/xquareinfra/adapter/in/container/dto/request/CreateNodeWithNginxDockerfileRequest.kt
@@ -3,7 +3,7 @@ package xquare.app.xquareinfra.adapter.`in`.container.dto.request
 import xquare.app.xquareinfra.adapter.out.external.github.client.dto.request.DockerfileRequest
 
 data class CreateNodeWithNginxDockerfileRequest(
-    val nodeVersion: Int,
+    val nodeVersion: String,
     val buildCommands: List<String> = emptyList(),
     override val builder: String = "node_with_nginx",
     override var buildDir: String = "/",
