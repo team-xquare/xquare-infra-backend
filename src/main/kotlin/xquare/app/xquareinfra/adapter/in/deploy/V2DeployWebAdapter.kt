8847c084d9909a509a78e5e28c248a39ab2a3850
강은찬 <kangeunchan080310@gmail.com>
2024-11-18 11:15:55 +0900

    feat : Implement deleteContainer endpoint for container deletion in V2DeployWebAdapter

--- src/main/kotlin/xquare/app/xquareinfra/adapter/in/deploy/V2DeployWebAdapter.kt
+++ src/main/kotlin/xquare/app/xquareinfra/adapter/in/deploy/V2DeployWebAdapter.kt
@@ -6,18 +6,24 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import xquare.app.xquareinfra.adapter.`in`.deploy.dto.request.DeleteContainerRequest
+import xquare.app.xquareinfra.adapter.`in`.deploy.dto.response.DeleteContainerResponse
+import xquare.app.xquareinfra.application.auth.port.out.SecurityPort
+import xquare.app.xquareinfra.application.deploy.port.`in`.DeployUseCase
 import java.util.UUID
 
 @RequestMapping("/v2/deploy")
 @RestController
-class V2DeployWebAdapter {
+class V2DeployWebAdapter(
+    private val deployUseCase: DeployUseCase,
+    private val securityPort: SecurityPort
+) {
     @DeleteMapping
     fun deleteContainer(
         @RequestParam("team_id")
         teamId: UUID,
         @RequestBody
         deleteContainerRequest: DeleteContainerRequest
-    ) {
-
+    ): DeleteContainerResponse {
+        return deployUseCase.deleteDeploy(teamId, securityPort.getCurrentUser(), deleteContainerRequest)
     }
 }
