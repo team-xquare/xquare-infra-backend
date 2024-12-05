eb7c1c084c80e4838876716f17e6d633581641dc
강은찬 <kangeunchan080310@gmail.com>
2024-12-06 08:40:54 +0900

    feat : Update deleteContainer method to use deployId as a path variable instead of request body

--- src/main/kotlin/xquare/app/xquareinfra/adapter/in/deploy/V2DeployWebAdapter.kt
+++ src/main/kotlin/xquare/app/xquareinfra/adapter/in/deploy/V2DeployWebAdapter.kt
@@ -1,13 +1,13 @@
 package xquare.app.xquareinfra.adapter.`in`.deploy
 
 import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import xquare.app.xquareinfra.adapter.`in`.deploy.dto.request.DeleteContainerRequest
 import xquare.app.xquareinfra.adapter.`in`.deploy.dto.response.DeleteContainerResponse
 import xquare.app.xquareinfra.application.auth.port.out.SecurityPort
 import xquare.app.xquareinfra.application.deploy.port.`in`.DeployUseCase
+import java.util.*
 
 @RequestMapping("/v2/deploy")
 @RestController
@@ -15,11 +15,11 @@ class V2DeployWebAdapter(
     private val deployUseCase: DeployUseCase,
     private val securityPort: SecurityPort
 ) {
-    @DeleteMapping
+    @DeleteMapping("/{deployId}")
     fun deleteContainer(
-        @RequestBody
-        deleteContainerRequest: DeleteContainerRequest
+        @PathVariable
+        deployId: UUID,
     ): DeleteContainerResponse {
-        return deployUseCase.deleteDeploy(securityPort.getCurrentUser(), deleteContainerRequest)
+        return deployUseCase.deleteDeploy(securityPort.getCurrentUser(), deployId)
     }
 }
