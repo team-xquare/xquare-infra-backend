bfb8a01dec1c3b0abd6de5b6f20d56f8484d64a0
강은찬 <kangeunchan080310@gmail.com>
2024-12-06 08:40:41 +0900

    feat : Remove DeleteDeployPort interface and its associated methods

--- src/main/kotlin/xquare/app/xquareinfra/application/deploy/port/out/DeleteDeployPort.kt
+++
@@ -1,8 +0,0 @@
-package xquare.app.xquareinfra.application.deploy.port.out
-
-import xquare.app.xquareinfra.adapter.`in`.deploy.dto.request.DeleteContainerRequest
-import java.util.*
-
-interface DeleteDeployPort {
-    fun deleteDeploy(teamId: UUID, deleteContainerRequest: DeleteContainerRequest)
-}
