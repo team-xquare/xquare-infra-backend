c6d5ccd4a4841829f3e09ad388c9606b84b56fe8
HyunSu1768 <azxcv1768@gmail.com>
2025-03-07 08:59:17 +0900

    bug :: 버그수정

--- src/main/kotlin/xquare/app/xquareinfra/infrastructure/exception/Exceptions.kt
+++ src/main/kotlin/xquare/app/xquareinfra/infrastructure/exception/Exceptions.kt
@@ -43,7 +43,7 @@ class BusinessLogicException(
         val USER_TEAM_BAD_REQUEST = BusinessLogicException(ErrorCodePrefixSuffix.XXX_BAD_REQUEST, DomainNames.USER_TEAM)
         val ALREADY_EXISTS_DEPLOY = BusinessLogicException(ErrorCodePrefixSuffix.ALREADY_EXISTS_XXX, DomainNames.DEPLOY)
         val DEPLOY_NOT_FOUND = BusinessLogicException(ErrorCodePrefixSuffix.XXX_NOT_FOUND, DomainNames.DEPLOY)
-        val DASHBOARD_NOT_FOUND = BusinessLogicException(ErrorCodePrefixSuffix.XXX_NOT_FOUND, DomainNames)
+        val DASHBOARD_NOT_FOUND = BusinessLogicException(ErrorCodePrefixSuffix.XXX_NOT_FOUND, DomainNames.DASHBOARD)
         val CREATE_DEPLOY_BAD_REQUEST = BusinessLogicException(ErrorCodePrefixSuffix.XXX_BAD_REQUEST, DomainNames.DEPLOY)
         val USER_ALREADY_EXISTS = BusinessLogicException(ErrorCodePrefixSuffix.ALREADY_EXISTS_XXX, DomainNames.USER)
         val CONTAINER_NOT_FOUND = BusinessLogicException(ErrorCodePrefixSuffix.XXX_NOT_FOUND, DomainNames.CONTAINER)
