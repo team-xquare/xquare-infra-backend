3ca3262b70f2b7606cf9315297a5e2d962e251d4
HyunSu1768 <azxcv1768@gmail.com>
2025-03-10 15:32:54 +0900

    feat :: snake case

--- src/main/kotlin/xquare/app/xquareinfra/adapter/out/external/github/client/dto/response/LoginAccessTokenResponse.kt
+++ src/main/kotlin/xquare/app/xquareinfra/adapter/out/external/github/client/dto/response/LoginAccessTokenResponse.kt
@@ -1,7 +1,11 @@
 package xquare.app.xquareinfra.adapter.out.external.github.client.dto.response
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy
+import com.fasterxml.jackson.databind.annotation.JsonNaming
+
+@JsonNaming(SnakeCaseStrategy::class)
 data class LoginAccessTokenResponse(
-    val access_token: String,
+    val accessToken: String,
     val scope: String,
-    val token_type: String,
+    val tokenType: String,
 )
